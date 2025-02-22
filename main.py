2d8db4fba96457e6a6f250ea3bfc7352d67c2917
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-02-22 07:28:49 -0500

    2025-02-22-07-28-49

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import duckdb
 
 def main():
     print("Hello from vscode-ide!")
-    get_fake_data()
 
+def get_fake_data(input_param: int):
+    """some defintion to test docs preview
 
-def get_fake_data():
+    Args:
+        input_param: 
+
+    Returns:
+        
+    """
     fake_data = duckdb.sql(query="select 1").fetchall()
     for f_data in fake_data:
         print(f_data, type(f_data), f_data[0], len(f_data))
@@ -14,7 +20,7 @@ def get_fake_data():
 
 
 def some_function(some_input_param_1: int, param_2: str) -> str:
-    return param_2 * some_input_param_1  
+    return param_2 * some_input_param_1
 
 
 if __name__ == "__main__":
